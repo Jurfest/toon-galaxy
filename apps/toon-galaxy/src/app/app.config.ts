4a1906fbf61223e0e7604fa712f78b2cfd3ece69
Jurfest <jurfest.apps@gmail.com>
2024-05-22 19:53:27 -0300

    chore: config initial toon-galaxy domain and lib

--- apps/toon-galaxy/src/app/app.config.ts
+++ apps/toon-galaxy/src/app/app.config.ts
@@ -1,14 +1,52 @@
-import { ApplicationConfig } from '@angular/core';
+import {
+  provideHttpClient,
+  withFetch,
+  withInterceptors,
+} from '@angular/common/http';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
+import { provideEffects } from '@ngrx/effects';
+import { provideState, provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { ENVIRONMENT_INITIALIZER } from '@angular/core';
 
 import { appRoutes } from './app.routes';
 
+import { provideToonGalaxyDomain } from '@toon-galaxy/toon-galaxy/domain';
+
 export const appConfig: ApplicationConfig = {
   providers: [
+    /**
+     * It's strongly recommended to enable fetch for applications that uses
+     * Server-Side Rendering and/or Static Site Generation SSG/Prerendering,
+     * for better performance and compatibility.
+     *
+     * Prefer withInterceptors and functional interceptors instead of
+     * withInterceptorsFromDi, as support for DI-provided interceptors may
+     * be phased out in a later release.
+     */
+    provideHttpClient(withInterceptors([]), withFetch()),
     provideClientHydration(),
     provideRouter(appRoutes),
     provideAnimationsAsync(),
+
+    // NgRx
+    provideStore(),
+    provideEffects([]),
+    ...(isDevMode() ? [provideStoreDevtools()] : []),
+    // provideState(),
+    provideToonGalaxyDomain(),
+
+    // Perform initialization, has to be last
+    {
+      provide: ENVIRONMENT_INITIALIZER,
+      multi: true,
+      useValue() {
+        // add init logic here...
+        // kickstart processes, trigger initial requests or actions, ...
+      },
+    },
   ],
 };
