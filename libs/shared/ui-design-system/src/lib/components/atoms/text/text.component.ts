b274f829e79e9c5b57d178c4ea099d2a8335e95a
Jurfest <jurfest.apps@gmail.com>
2024-05-23 14:02:25 -0300

    feat: scaffold text atomic cmp

---
+++ libs/shared/ui-design-system/src/lib/components/atoms/text/text.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'design-system-text',
+  standalone: true,
+  imports: [],
+  templateUrl: './text.component.html',
+  styleUrl: './text.component.scss',
+})
+export class TextComponent {}
