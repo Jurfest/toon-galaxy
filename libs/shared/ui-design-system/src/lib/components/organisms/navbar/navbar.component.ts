017f35ec8c912db328f4e2ec67545c9c63015b4e
Jurfest <jurfest.apps@gmail.com>
2024-06-07 19:51:40 -0300

    feat: add icons to navbar

--- libs/shared/ui-design-system/src/lib/components/organisms/navbar/navbar.component.ts
+++ libs/shared/ui-design-system/src/lib/components/organisms/navbar/navbar.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
 
 import { LogoComponent } from '../../atoms/logo/logo.component';
+import { IconComponent } from '../../atoms/icon/icon.component';
 
 @Component({
   selector: 'design-system-navbar',
   standalone: true,
-  imports: [CommonModule, LogoComponent],
+  imports: [CommonModule, LogoComponent, IconComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
