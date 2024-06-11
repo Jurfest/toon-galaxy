774bfe7d2e8b6d15db92456f41d4c590d8388e44
Jurfest <jurfest.apps@gmail.com>
2024-06-11 16:26:07 -0300

    feat: bind favorites to card-list

--- libs/shared/ui-design-system/src/lib/components/organisms/card-list/card-list.component.ts
+++ libs/shared/ui-design-system/src/lib/components/organisms/card-list/card-list.component.ts
@@ -1,16 +1,20 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 
 import { Card } from '../../../models/card';
+import { IconComponent } from '../../atoms/icon/icon.component';
 
 @Component({
   selector: 'design-system-card-list',
   standalone: true,
-  imports: [],
+  imports: [IconComponent],
   templateUrl: './card-list.component.html',
   styleUrl: './card-list.component.scss',
 })
 export class CardListComponent {
-  // @Input() cards: Card[] = [];
+  cardList = input.required<Card[]>();
+  favCardUpdate = output<Card>();
 
-  characterList = input.required<Card[]>();
+  onToggleFavCard(card: Card): void {
+    this.favCardUpdate.emit(card);
+  }
 }
