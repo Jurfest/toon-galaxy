90983c08abeeb0c6580ef0f260018462caf8a10d
Jurfest <jurfest.apps@gmail.com>
2024-06-11 13:01:32 -0300

    refactor: bind events and properties and remove not necessary imports

--- libs/shared/ui-design-system/src/lib/components/molecules/toggle-button/toggle-button.component.ts
+++ libs/shared/ui-design-system/src/lib/components/molecules/toggle-button/toggle-button.component.ts
@@ -1,11 +1,10 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatRippleModule } from '@angular/material/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { IconComponent } from '../../atoms/icon/icon.component';
 
@@ -16,7 +15,6 @@ import { IconComponent } from '../../atoms/icon/icon.component';
     CommonModule,
     IconComponent,
     MatRippleModule,
-    MatToolbarModule,
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
@@ -45,9 +43,37 @@ import { IconComponent } from '../../atoms/icon/icon.component';
 export class ToggleButtonComponent {
   totalFavoriteCharacters = input.required<number | null>();
   isHandset = input.required<boolean | null>();
-  active = 'home';
+  activeTab = input.required<string | null>();
+  toggleActive = output<string>();
 
-  toggleActive(tab: string): void {
-    this.active = tab;
+  onToggle(tabName: string): void {
+    this.toggleActive.emit(tabName);
   }
+
+  //
+  //
+  // // Old
+  // // @Input() checked = false;
+  // // // For two way binding the output property had to have same name of the input
+  // // // plus 'change' and emit same type of data
+  // // @Output() checkedChange = new EventEmitter<boolean>();
+
+  // // New
+  // checked = model(false);
+
+  // activeTabName = model.required<string>();
+
+  // handleClick(tabName: string): void {
+  //   this.activeTabName.update(() => tabName);
+  // }
+
+  // // Other properties
+  // indeterminate = false;
+  // labelPosition: 'before' | 'after' = 'after';
+  // disabled = false;
+
+  // // Example of utilization of ModelSignal as an Writable Signal
+  // handleClick(): void {
+  //   this.checked.update((c) => !c);
+  // }
 }
--- libs/shared/ui-design-system/src/lib/components/organisms/navbar/navbar.component.ts
+++ libs/shared/ui-design-system/src/lib/components/organisms/navbar/navbar.component.ts
@@ -1,52 +1,17 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { CommonModule } from '@angular/common';
 import { Component, inject, input } from '@angular/core';
-import { MatRippleModule } from '@angular/material/core';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatMenuModule } from '@angular/material/menu';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, shareReplay } from 'rxjs';
 
-import { IconComponent } from '../../atoms/icon/icon.component';
 import { LogoComponent } from '../../atoms/logo/logo.component';
 import { ToggleButtonComponent } from '../../molecules/toggle-button/toggle-button.component';
-import { map, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'design-system-navbar',
   standalone: true,
-  imports: [
-    CommonModule,
-    LogoComponent,
-    ToggleButtonComponent,
-    IconComponent,
-    MatRippleModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatMenuModule,
-  ],
+  imports: [CommonModule, LogoComponent, ToggleButtonComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
-  animations: [
-    trigger('numberChange', [
-      transition(':increment', [
-        style({ transform: 'translateY(-20%)', opacity: 0 }),
-        animate(
-          '300ms ease-out',
-          style({ transform: 'translateY(0)', opacity: 1 }),
-        ),
-      ]),
-      transition(':decrement', [
-        style({ transform: 'translateY(20%)', opacity: 0 }),
-        animate(
-          '300ms ease-out',
-          style({ transform: 'translateY(0)', opacity: 1 }),
-        ),
-      ]),
-    ]),
-  ],
 })
 export class NavbarComponent {
   totalFavoriteCharacters = input.required<number | null>();
