a6160c347eb91f11f02f3b435045c8af40fe90d0
Jurfest <jurfest.apps@gmail.com>
2024-06-14 19:00:43 -0300

    feat: add navigation between tabs

--- libs/shared/ui-design-system/src/lib/components/organisms/navbar/navbar.component.ts
+++ libs/shared/ui-design-system/src/lib/components/organisms/navbar/navbar.component.ts
@@ -6,11 +6,12 @@ import {
   inject,
   input,
 } from '@angular/core';
-import { map, shareReplay } from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map, Observable, shareReplay, startWith } from 'rxjs';
 
+import { TabName } from '../../../models/tab-name';
 import { LogoComponent } from '../../atoms/logo/logo.component';
 import { ToggleButtonComponent } from '../../molecules/toggle-button/toggle-button.component';
-import { TabName } from '../../../models/tab-name';
 
 @Component({
   selector: 'design-system-navbar',
@@ -22,9 +23,18 @@ import { TabName } from '../../../models/tab-name';
 })
 export class NavbarComponent {
   totalFavoriteCharacters = input.required<number | null>();
-  active = TabName.Home;
 
+  private router = inject(Router);
   private breakpointObserver = inject(BreakpointObserver);
+
+  active$: Observable<TabName> = this.router.events.pipe(
+    filter((event) => event instanceof NavigationEnd),
+    startWith(new NavigationEnd(0, this.router.url, this.router.url)),
+    map(() => {
+      const segments = this.router.url.split('/');
+      return segments.includes('favorites') ? TabName.Favorites : TabName.Home;
+    }),
+  );
 
   /**
    * The Breakpoints.Handset media query matches small viewport
@@ -36,8 +46,8 @@ export class NavbarComponent {
     shareReplay(),
   );
 
-  toggleActive(tab: TabName): void {
-    this.active = tab;
+  navigateToTabName(tabName: TabName): void {
+    this.router.navigate([`manage-characters/${tabName}`]);
   }
 }
 
