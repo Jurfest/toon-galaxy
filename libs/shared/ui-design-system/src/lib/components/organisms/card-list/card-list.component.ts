567d3b40f35b42a123c08b6204ee95e953df9897
Jurfest <jurfest.apps@gmail.com>
2024-06-18 15:09:37 -0300

    refactor: move click to icon bg

--- libs/shared/ui-design-system/src/lib/components/organisms/card-list/card-list.component.ts
+++ libs/shared/ui-design-system/src/lib/components/organisms/card-list/card-list.component.ts
@@ -1,26 +1,12 @@
 import { Component, input, output } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
 
 import { Card } from '../../../models/card';
-import { HeadingComponent } from '../../atoms/heading/heading.component';
-import { IconComponent } from '../../atoms/icon/icon.component';
-import { TextComponent } from '../../atoms/text/text.component';
-import { MatInput } from '@angular/material/input';
 import { CardComponent } from '../../molecules/card/card.component';
 
 @Component({
   selector: 'design-system-card-list',
   standalone: true,
-  imports: [
-    TextComponent,
-    HeadingComponent,
-    IconComponent,
-    CardComponent,
-    MatCardModule,
-    MatButtonModule,
-    MatInput,
-  ],
+  imports: [CardComponent],
   templateUrl: './card-list.component.html',
   styleUrl: './card-list.component.scss',
 })
