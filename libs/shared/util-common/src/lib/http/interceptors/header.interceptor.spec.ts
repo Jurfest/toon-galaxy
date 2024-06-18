1a2de9b4481111968267a195f95332f0a8b5136e
Jurfest <jurfest.apps@gmail.com>
2024-06-18 01:46:18 -0300

    test: verify header interceptor behavior

--- libs/shared/util-common/src/lib/http/interceptors/header.interceptor.spec.ts
+++ libs/shared/util-common/src/lib/http/interceptors/header.interceptor.spec.ts
@@ -1,11 +1,6 @@
-import { TestBed } from '@angular/core/testing';
 import {
   HttpClient,
-  HttpHandler,
-  HttpHandlerFn,
   HttpInterceptorFn,
-  HttpRequest,
-  HttpResponse,
   provideHttpClient,
   withInterceptors,
 } from '@angular/common/http';
@@ -13,8 +8,9 @@ import {
   HttpTestingController,
   provideHttpClientTesting,
 } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
 import { headerInterceptor } from './header.interceptor';
-import { Observable, of } from 'rxjs';
 
 describe('headerInterceptor', () => {
   const interceptor: HttpInterceptorFn = (req, next) =>
@@ -41,75 +37,15 @@ describe('headerInterceptor', () => {
     expect(interceptor).toBeTruthy();
   });
 
-  // it('should add Content-Type header if not present', () => {
-  //   const mockReq = new HttpRequest('GET', '/api/data');
-  //   const mockHttpHandler: HttpHandlerFn = (
-  //     req: HttpRequest<any>,
-  //   ): Observable<HttpResponse<any>> => {
-  //     // Mock responses based on request URL, method, etc.
-  //     if (req.url.endsWith('/api/data') && req.method === 'GET') {
-  //       const mockData = { message: 'Mocked response' };
-  //       return of(new HttpResponse({ status: 200, body: mockData }));
-  //     } else {
-  // Handle other requests or return mock errors as needed
-  //       return of(new HttpResponse({ status: 404, statusText: 'Not Found' }));
-  //     }
-  //   };
-  //   // Observable<HttpEvent<unknown>>;
-  //   const intercepted = headerInterceptor(mockReq, mockHttpHandler);
-
-  //   const intercepted2 = interceptor(mockReq, mockHttpHandler);
-  //   // const intercepted = interceptor(mockReq, (req) => of(mockReq));
-  //   // const intercepted = interceptor(mockReq, (req) => mockReq);
-  //   // Assert the headers were modified as expected
-  //   // expect(intercepted.request.headers.has('Content-Type')).toBe(true);
-  //   expect(intercepted.request.headers.has('Content-Type')).toBe(true);
-  //   expect(intercepted2.request.headers.get('Content-Type')).toEqual(
-  //     'application/json',
-  //   );
-  // });
-
-  it('should add auth headers ', () => {
-    //arrange
+  it('should add Content-Type header if not present', () => {
     const url = '/mockendpoint';
 
-    //act
     httpClient.get(url).subscribe();
 
-    // assert
     const req = httpTestingController.expectOne(url);
+    expect(req.request.headers.has('Content-Type')).toBe(true);
     expect(req.request.headers.get('Content-Type')).toEqual('application/json');
+
+    req.flush({ message: 'Mocked response' });
   });
 });
-
-// describe('AuthInterceptor', () => {
-
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       providers: [
-//         provideHttpClient(withInterceptors([headerInterceptor])),
-//         provideHttpClientTesting(),
-//       ],
-//     });
-//     httpTestingController = TestBed.inject(HttpTestingController);
-//     httpClient = TestBed.inject(HttpClient);
-//   });
-
-//   afterEach(() => {
-//     httpTestingController.verify();
-//   });
-
-//   it('should add auth headers ', () => {
-//     //arrange
-//     const url = '/mockendpoint';
-
-//     //act
-//     httpClient.get(url).subscribe();
-
-//     // assert
-//     const req = httpTestingController.expectOne(url);
-//     expect(req.request.headers.get('Authorization')).toEqual(
-//       'Bearer [the token]',
-//     );
-//   });
-// });
