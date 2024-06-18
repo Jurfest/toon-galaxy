362bee947fe64db83b9dc54c5982bd7f47a124f5
Jurfest <jurfest.apps@gmail.com>
2024-06-18 16:43:57 -0300

    chore: remove unused comments and code

--- libs/shared/ui-design-system/src/lib/components/molecules/card/card.component.ts
+++ libs/shared/ui-design-system/src/lib/components/molecules/card/card.component.ts
@@ -1,40 +1,15 @@
-import {
-  animate,
-  state,
-  style,
-  transition,
-  trigger,
-} from '@angular/animations';
 import { CommonModule } from '@angular/common';
 import { Component, input, output } from '@angular/core';
 
 import { Card } from '../../../models/card';
-import { IconComponent } from '../../atoms/icon/icon.component';
 import { IconHighlightComponent } from '../icon-highlight/icon-highlight.component';
 
 @Component({
   selector: 'design-system-card',
   standalone: true,
-  imports: [CommonModule, IconComponent, IconHighlightComponent],
+  imports: [CommonModule, IconHighlightComponent],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
-  animations: [
-    trigger('toggleHeartBackground', [
-      state(
-        'false',
-        style({
-          backgroundColor: 'rgba(255, 255, 255, 0.01)',
-        }),
-      ),
-      state(
-        'true',
-        style({
-          backgroundColor: '#ffffff',
-        }),
-      ),
-      transition('transparent <=> white', [animate('0.5s ease-in-out')]),
-    ]),
-  ],
 })
 export class CardComponent {
   card = input.required<Card>();
