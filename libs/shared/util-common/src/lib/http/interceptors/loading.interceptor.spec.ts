439b8bb08507f840b0ed5a7274a01431f75c457c
Jurfest <jurfest.apps@gmail.com>
2024-06-24 18:56:38 -0300

    test: verify loading interceptor

--- libs/shared/util-common/src/lib/http/interceptors/loading.interceptor.spec.ts
+++ libs/shared/util-common/src/lib/http/interceptors/loading.interceptor.spec.ts
@@ -1,17 +1,57 @@
+import {
+  HttpClient,
+  HttpInterceptorFn,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
 
+import { LoadingFacade } from '../+state/loading/loading.facade';
 import { loadingInterceptor } from './loading.interceptor';
 
-describe('loadInterceptor', () => {
+describe('LoadingInterceptor', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let loadingFacadeMock: Partial<LoadingFacade>;
+
   const interceptor: HttpInterceptorFn = (req, next) =>
     TestBed.runInInjectionContext(() => loadingInterceptor(req, next));
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    loadingFacadeMock = {
+      start: jest.fn().mockReturnValue('requestId'),
+      stop: jest.fn(),
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(withInterceptors([loadingInterceptor])),
+        provideHttpClientTesting(),
+        { provide: LoadingFacade, useValue: loadingFacadeMock },
+      ],
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(interceptor).toBeTruthy();
+  });
+
+  it('should start and stop the loading indicator', () => {
+    httpClient.get('/api/data').subscribe();
+    const req = httpTestingController.expectOne('/api/data');
+    expect(loadingFacadeMock.start).toHaveBeenCalled();
+    req.flush({});
+    expect(loadingFacadeMock.stop).toHaveBeenCalledWith('requestId');
   });
 });
