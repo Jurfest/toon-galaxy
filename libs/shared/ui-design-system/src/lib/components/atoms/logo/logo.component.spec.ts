c7be8e99e14c3e393eabfb8b64b694b3c84dd259
Jurfest <jurfest.apps@gmail.com>
2024-06-24 14:47:41 -0300

    test: add test cases to logo

--- libs/shared/ui-design-system/src/lib/components/atoms/logo/logo.component.spec.ts
+++ libs/shared/ui-design-system/src/lib/components/atoms/logo/logo.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { LogoComponent } from './logo.component';
 
@@ -18,5 +19,26 @@ describe('LogoComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display the correct image source', () => {
+    const imageSrcWithoutDot = '/assets/images/rick-morty-logo.svg';
+    fixture.detectChanges();
+    const imgElement = fixture.debugElement.query(By.css('img')).nativeElement;
+    expect(imgElement.src).toContain(imageSrcWithoutDot);
+  });
+
+  it('should display the correct alt text', () => {
+    const altText = 'Rick and Morty Logo';
+    fixture.detectChanges();
+    const imgElement = fixture.debugElement.query(By.css('img')).nativeElement;
+    expect(imgElement.alt).toBe(altText);
+  });
+
+  it('should set the priority attribute on the image', () => {
+    const priority = true;
+    fixture.detectChanges();
+    const imgElement = fixture.debugElement.query(By.css('img')).nativeElement;
+    expect(imgElement.hasAttribute('priority')).toBe(priority);
   });
 });
